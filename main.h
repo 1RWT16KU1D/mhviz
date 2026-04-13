668a414b3261d376759fbd09c751d5c30594a730
1RWT16KU1D <darkc8710@gmail.com>
2026-04-14 00:09:16 +0530

    Fixed Slight Visual Annoyance of Uneven Width

--- main.h
+++ main.h
@@ -9,11 +9,17 @@
 #define TRUE true
 #define FALSE false
 
-#define NELEMS 50
+#define NELEMS 67
 #define MAX_VALUE 50
 
 #define HEIGHT ((20 * MAX_VALUE) + 250)
-#define WIDTH 2500
+#define INITIAL_WIDTH 2500
+
+#if ((INITIAL_WIDTH % NELEMS) != 0)
+#define WIDTH (INITIAL_WIDTH - (INITIAL_WIDTH % NELEMS))
+#else
+#define WIDTH INITIAL_WIDTH
+#endif
 
 #define HEIGHT_FACTOR 20
 #define BAR_WIDTH (WIDTH / NELEMS)
