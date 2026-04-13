70ffc40168030484f26a0dd3c21dd391f578e2cc
1RWT16KU1D <darkc8710@gmail.com>
2026-04-14 00:01:10 +0530

    Use struct instead of raw rectangle data

--- main.h
+++ main.h
@@ -5,12 +5,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NELEMS 100
+// For MY convenience
+#define TRUE true
+#define FALSE false
+
+#define NELEMS 50
 #define MAX_VALUE 50
 
 #define HEIGHT ((20 * MAX_VALUE) + 250)
 #define WIDTH 2500
 
+#define HEIGHT_FACTOR 20
 #define BAR_WIDTH (WIDTH / NELEMS)
 
 typedef struct
--- visualizer.c
+++ visualizer.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
+// Global initializations
 static int *arr;
+Rectangle rectangles[NELEMS];
 Bar bars[NELEMS];
 
-static int *setArray(int arr[], int n)
+#pragma region Data Initialization
+static int *initArray(int arr[], int n)
 {
     arr = (int *)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
@@ -11,24 +14,47 @@ static int *setArray(int arr[], int n)
     return arr;
 }
 
-static inline void drawBar(int x, int y, int width, int height)
+static void initRectangleData(void)
 {
-    DrawRectangle(x, y, width, height, WHITE);
-    DrawRectangleLines(x, y, width, height, BLACK);
+    for (int i = 0; i < NELEMS; i++)
+    {
+        rectangles[i].height = HEIGHT_FACTOR * arr[i];
+        rectangles[i].width = BAR_WIDTH;
+        rectangles[i].x = BAR_WIDTH * i;
+        rectangles[i].y = HEIGHT - (HEIGHT_FACTOR * arr[i]);
+    }
+}
+
+static void initBarData(void)
+{
+    for (int i = 0; i < NELEMS; i++)
+    {
+        bars[i].rect = rectangles[i];
+        bars[i].color = WHITE;
+        bars[i].active = TRUE;
+    }
+}
+#pragma endregion
+
+#pragma region Drawing Logic
+static inline void drawBar(Rectangle rect, Color color)
+{
+    DrawRectangleRec(rect, color);
+    DrawRectangleLines(rect.x, rect.y, rect.width, rect.height, BLACK);
 }
 
 void drawBars(int n)
 {
     for (int i = 0; i < n; i++)
-    {
-        drawBar(BAR_WIDTH * i, HEIGHT - (20 * arr[i]), BAR_WIDTH, 20 * arr[i]);
-    }
+        drawBar(bars[i].rect, bars[i].color);
 }
 
 void visualize(void)
 {
     SetRandomSeed((unsigned int)time(NULL));
-    arr = setArray(arr, NELEMS);
+    arr = initArray(arr, NELEMS);
+    initRectangleData();
+    initBarData();
     InitWindow(WIDTH, HEIGHT, "mhviz - A Sorting Algorithm Visualizer");
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -41,3 +67,4 @@ void visualize(void)
     }
     CloseWindow();
 }
+#pragma endregion
